1fa2ad779185368dae23824dd162cba146857be6
Saksham Gupta <saksham@sakshams-air.mynetworksettings.com>
2026-03-13 08:33:34 -0400

    Initial commit

--- 03d12/Task5.py
+++ 03d12/Task5.py
@@ -6,3 +6,12 @@ width = float(input("Enter width: "))
 a = length * width
 
 print(f"Area of the rectangle is {a}")
+
+
+
+'''
+Output:
+Enter length: 20
+Enter width: 7
+Area of the rectangle is 140.0
+'''
