9bd90cc36dedceed67aff3ba4eef90d0a2513302
Saksham Gupta <saksham@sakshams-air.mynetworksettings.com>
2026-03-16 03:19:16 -0400

    03d13

---
+++ 03d13/Task1.py
@@ -0,0 +1,17 @@
+#Add files branch: Test file 1
+
+length = float(input("Enter length: "))
+width = float(input("Enter width: "))
+
+a = length * width
+
+print(f"Area of the rectangle is {a}")
+
+
+
+'''
+Output:
+Enter length: 20
+Enter width: 7
+Area of the rectangle is 140.0
+'''
