6b75032f0ce306dc01062595523d80b871268e1c
Saksham Gupta <saksham@sakshams-air.mynetworksettings.com>
2026-03-16 03:48:31 -0400

    Task2

---
+++ 03d13/Task2.py
@@ -0,0 +1,66 @@
+#Arithmetic operators
+
+#Addition
+x = 5 
+y = 6 
+print("Addition:", x + y + 6) # Output: Addition: 17
+
+#Subtraction
+a = 10 
+b = 5
+print("Subtraction:", a - b) #Output: Subtraction: 5
+
+#Multiplication
+c = 12
+d = 6
+print("Multiplication:", c * d) #Output: Multiplication: 72
+
+#Division
+e = 2187
+f = 3
+print("Division:", int(e / f)) #Output: Division: 729
+
+#Modulus
+g = 10 
+h = 9
+i = 4
+j = 2
+print("Modulus:", g % i) # 10 % 4 = 2
+print("Modulus:", g % j) # 10 % 2 = 0
+print("Modulus:", h % i) # 9 % 4 = 1
+print("Modulus:", h % j) # 9 % 2 = 1
+'''
+Output:
+
+Modulus: 2
+Modulus: 0
+Modulus: 1
+Modulus: 1
+
+'''
+
+#Exponentiation
+k = 2
+m = 3 * k
+print("Exponentiation:", k ** m) #Output: Exponentiation: 64
+
+#Floor division
+n = 21
+o = 5
+print("Floor division:", n // o) #Output: Floor division: 4
+
+'''
+Entire Output:
+
+Addition: 17
+Subtraction: 5
+Multiplication: 72
+Division: 729
+Modulus: 2
+Modulus: 0
+Modulus: 1
+Modulus: 1
+Exponentiation: 64
+Floor division: 4
+
+'''
