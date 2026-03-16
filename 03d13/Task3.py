183dd1f9ff14be901cbf3f83f6c31d89abb4a99d
Saksham Gupta <saksham@sakshams-air.mynetworksettings.com>
2026-03-16 03:57:52 -0400

    Task3

---
+++ 03d13/Task3.py
@@ -0,0 +1,23 @@
+#Comparison operators
+
+x = 15
+y = 15
+
+print("Equal:", x == y)
+print("Not equal:", x != y)
+print("Greater than:", x > y)
+print("Less than:", x < y)
+print("Greater than or equal to:", 4 >= y)
+print("Less than or equal to:", x <= 20)
+
+'''
+Output:
+
+Equal: True
+Not equal: False
+Greater than: False
+Less than: False
+Greater than or equal to: False
+Less than or equal to: True
+
+'''
