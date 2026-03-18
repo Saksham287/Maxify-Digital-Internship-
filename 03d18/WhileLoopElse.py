f728fe282ff2d286d3e50d006b3509e0f6e0d9d9
Saksham Gupta <saksham@sakshams-air.mynetworksettings.com>
2026-03-18 18:55:01 -0400

    While else loop

---
+++ 03d18/WhileLoopElse.py
@@ -0,0 +1,26 @@
+# While loops: While-else
+
+i = 1
+
+while i < 20:
+    print(i)
+    i += 2
+else: 
+    print("All done")
+    
+'''
+Output:
+
+1
+3
+5
+7
+9
+11
+13
+15
+17
+19
+All done
+
+'''
