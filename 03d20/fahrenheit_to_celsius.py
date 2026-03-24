0cbd98cd67703f11bb841c738636d531530061b8
Saksham Gupta <saksham@Sakshams-MacBook-Air.local>
2026-03-24 19:54:31 -0400

    f_to_c

---
+++ 03d20/fahrenheit_to_celsius.py
@@ -0,0 +1,16 @@
+# fahrenheit to celsius using function
+
+def f_to_c(f):
+  return int((f - 32) * 5 / 9)      # Need to learn how to round it to nearest 2 decimals
+
+print(f_to_c(77))
+print(f_to_c(49))
+print(f_to_c(97))
+
+'''
+Output: 
+
+25
+9
+36
+'''
